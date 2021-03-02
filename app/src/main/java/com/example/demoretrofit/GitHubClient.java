fae9dda0918ebe5abc78d75b7c413b581ffe6f0d
moon <erman_utegenov@mail.ru>
2021-03-03 00:24:43 +0600

    git commit

---
+++ app/src/main/java/com/example/demoretrofit/GitHubClient.java
@@ -0,0 +1,15 @@
+package com.example.demoretrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GitHubClient {
+
+        @GET("/users/{user}/repos")
+        Call<List<GitHubRepo>> reposForUser(
+                @Path("user") String user
+        );
+}
---
+++ app/src/main/java/com/example/demoretrofit/GitHubRepo.java
@@ -0,0 +1,17 @@
+package com.example.demoretrofit;
+
+public class GitHubRepo {
+    private int id;
+    private String name;
+
+    public GitHubRepo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
